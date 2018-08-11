dca76b21e3af40f451e2c5ebb2b088a84ef76583
Sunil Bahadur Bist <sunilbdrbist@gmail.com>
2018-08-11 21:54:57 +0545

    CrudRepositary

--- src/main/java/np/com/sunilbist/crudapp/repositary/StudentRepositary.java
+++ src/main/java/np/com/sunilbist/crudapp/repositary/StudentRepositary.java
@@ -1,12 +1,11 @@
 package np.com.sunilbist.crudapp.repositary;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import np.com.sunilbist.crudapp.entity.Student;
 
 @Repository
-public interface StudentRepositary extends JpaRepository<Student, Integer> {
+public interface StudentRepositary extends CrudRepository<Student, Integer> {
 
 }
